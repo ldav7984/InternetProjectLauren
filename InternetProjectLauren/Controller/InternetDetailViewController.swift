ed5498ec999dd05c7530e225e0299816caa984ad
Davis <ldav7984@740s-pl-109.local>
2018-12-14 12:54:27 -0700

    Coded the method to load PDF's.

--- InternetProjectLauren/Controller/InternetDetailViewController.swift
+++ InternetProjectLauren/Controller/InternetDetailViewController.swift
@@ -59,12 +59,18 @@ public class InternetDetailViewController: UIViewController
     
     private func loadURL(webAddress: String) -> Void
     {
-        
+        let currentURL = URL(string: webAddress)
+        let currentWebRequest = URLRequest(url: currentURL!)
+        webViewer.load(currentWebRequest)
     }
 
     private func loadPDF() -> Void
     {
-        
+        if let contentPDF = Bundle.main.url(forResource: "Internet Screen Topics", withExtension: "pdf", subdirectory: nil, localization: nil)
+        {
+            let requestedPDF = NSURLRequest(url: contentPDF)
+            webViewer.load(requestedPDF as URLRequest)
+        }
     }
     /*
     // MARK: - Navigation
