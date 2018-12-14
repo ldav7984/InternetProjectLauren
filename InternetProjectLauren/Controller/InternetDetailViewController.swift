65d05c4009c309f65941aaaf39603796eae47e8a
Davis <ldav7984@740s-pl-109.local>
2018-12-14 12:49:02 -0700

    Added methdos to the Internet detail view controller.

--- InternetProjectLauren/Controller/InternetDetailViewController.swift
+++ InternetProjectLauren/Controller/InternetDetailViewController.swift
@@ -7,18 +7,65 @@
 //
 
 import UIKit
+import WebKit
 
 public class InternetDetailViewController: UIViewController
 {
-
+    @IBOutlet weak var webViewer: WKWebView!
+    @IBOutlet weak var screenTitle: UILabel!
+    
+    
+    var detailTitle : String?
+    {
+        didSet
+        {
+            updateView()
+        }
+    }
+    
+    var detailAddress : String?
+    {
+        didSet
+        {
+            updateView()
+        }
+    }
+    
+    
+    private func updateView() -> Void
+    {
+        if (detailTitle != nil && screenTitle != nil && webViewer != nil)
+        {
+            if (detailTitle?.range(of: "Definitions", options: .caseInsensitive) != nil)
+            {
+                loadPDF()
+            }
+            else
+            {
+                if(detailAddress != nil)
+                {
+                    loadURL(webAddress: detailAddress!)
+                }
+            }
+            screenTitle?.text = detailTitle
+        }
+    }
+    
     public override func viewDidLoad() -> Void
     {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        updateView()
     }
     
+    private func loadURL(webAddress: String) -> Void
+    {
+        
+    }
 
+    private func loadPDF() -> Void
+    {
+        
+    }
     /*
     // MARK: - Navigation
 
